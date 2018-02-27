d7cb279b3803c3c1583ce8c1b92af508a79e305f
msc2913 <msc2913@g.coppellisd.com>
2018-02-26 19:42:49 -0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,92 @@
+//This is super redundant because it is not working properly . _ .
+
+
+function showHide1() {
+	var x = document.getElementById("blog1");
+	if(x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function showHide2() {
+	var x = document.getElementById("blog2");
+	if(x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function showHide3() {
+	var x = document.getElementById("blog3");
+	if(x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function showHide4() {
+	var x = document.getElementById("blog4");
+	if(x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function showHide5() {
+	var x = document.getElementById("blog5");
+	if(x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function showHide6() {
+	var x = document.getElementById("blog6");
+	if(x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function showHide7() {
+	var x = document.getElementById("blog7");
+	if(x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function showHide8() {
+	var x = document.getElementById("blog8");
+	if(x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function showHide9() {
+	var x = document.getElementById("blog9");
+	if(x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function showHide10() {
+	var x = document.getElementById("blog10");
+	if(x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
